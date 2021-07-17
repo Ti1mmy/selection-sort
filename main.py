586fc6065d55b93f11b3c5f6059fd14b40c694be
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-07-17 00:37:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def swap(a, b):
+    temp = my_list[a]
+    my_list[a] = my_list[b]
+    my_list[b] = temp
+
+my_list = [1, 3, 5, 8, 2, 9, 7]
+
+for i in range(len(my_list)):
+    min_index = i
+    for j in range(i + 1, len(my_list)):
+        if my_list[min_index] > my_list[j]:
+            min_index = j
+    swap(i, min_index)
+
+print(my_list)
